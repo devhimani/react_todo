7f5093e4fd6d3fb253df54dfbd9349c84255c8f2
himanigupta <gupta.himani44@gmail.com>
2024-11-12 02:16:39 -0800

    input_button_map_index added in todo list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
-import Input from "./components/Input";
+import { useState } from "react";
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleInput = (event) => {
+    setInput(event.target.value);
+    // console.log(event);
+  };
+
+  const handleAdd = () => {
+    // setTodos([input, ...todos]);
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
+  handleSubmitForm = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+  };
+
   return (
     <div>
-      <Input />
-
+      <form onSubmit={handleSubmitForm}>
+        <input
+          type="text"
+          value={input}
+          placeholder="Enter text"
+          onChange={handleInput}
+        />
+        <button onClick={handleAdd}>Add</button>
+      </form>
       <div>
-        <input type="text" className="input" placeholder="Enter text"></input>
-        <button>Add</button>
+        {/* <input
+          type="text"
+          value={input}
+          className="input"
+          placeholder="Enter text"
+          onChange={handleInput}
+        /> */}
+        {/* <button onClick={handleAdd}>Add</button> */}
+        {/* <input type="submit" onClick={handleAdd} value="Add" /> */}
       </div>
-
       <div>
         <ul>
-          <li>Task 1</li>
-          <li>Task 2 </li>
-          <li>Task 3</li>
+          {todos.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
         </ul>
       </div>
     </div>
