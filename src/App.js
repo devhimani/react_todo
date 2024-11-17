0cdf69cc69a21940f17626ff30130f74487d61be
himanigupta <gupta.himani44@gmail.com>
2024-11-17 03:30:47 -0800

    edit update button added with checking index

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 const App = () => {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+  const [newTodoValue, setNewTodoValue] = useState("");
 
   const handleInput = (event) => {
     setInput(event.target.value);
@@ -27,6 +29,13 @@ const App = () => {
     setTodos(todos.filter((_todo, index) => index !== clickedIndex));
   };
 
+  const handleUpdateTodo = (clickedIndex) => {
+    setTodos(
+      todos.map((todo, index) => (clickedIndex === index ? newTodoValue : todo)) // 6 to update
+    );
+    setEditIndex(null); // 7 to  remove input box  after updating value.
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmitForm}>
@@ -41,10 +50,39 @@ const App = () => {
       <div>
         <ul>
           {todos.map((todo, index) => (
-            // <li key={index}>{todo}</li>
             <li key={index} className="todo-item">
-              <p>{todo}</p>
-              <button onClick={() => handleDelete(index)}>Delete</button>
+              {editIndex === index ? (
+                <input
+                  type="text"
+                  value={newTodoValue}
+                  onChange={(e) => setNewTodoValue(e.target.value)} //
+                />
+              ) : (
+                <p>{todo}</p>
+              )}
+              {/* // 2 check the editIndex to show input box or todo list 
+               4 add value = newTodoValue 
+               6 add on change to target the setNewTodoValue */}
+
+              <div>
+                {editIndex === index ? ( // 7 now handler added with index to update newTodoValue
+                  <button onClick={() => handleUpdateTodo(index)}>
+                    Update
+                  </button> // 3 check the editindex to  show update button.
+                ) : (
+                  <button
+                    onClick={() => {
+                      setEditIndex(index);
+                      setNewTodoValue(todo); // 5 set the old value in input field with todo
+                    }}
+                  >
+                    Edit
+                  </button>
+                )}
+                {/* // 1 setEditindex is imp to show inpu tbox by clicking on edit
+                imp */}
+                <button onClick={() => handleDelete(index)}>Delete</button>
+              </div>
             </li>
           ))}
         </ul>
